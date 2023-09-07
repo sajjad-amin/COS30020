9acc067475ec6da7aff55463730a15de62bf42fa
sajjad-amin <sayem.1998.bd@gmail.com>
2023-09-07 13:52:53 +0600

    meta content added

--- inc/header.php
+++ inc/header.php
@@ -9,9 +9,11 @@ function getCurrentPage(){
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta charset="utf-8" />
+    <meta name="description" content="Advanced Web Development" />
+    <meta name="keywords" content="PHP" />
+    <meta name="author" content="Md Radif Rafayet Chowdhury" />
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>
         <?php
